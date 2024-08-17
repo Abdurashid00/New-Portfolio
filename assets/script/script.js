0778cd4929131ef88f55ec0b3e3c875938858781
Abdurashid <khaidarovabdurashid@gmail.com>
2024-08-17 15:14:48 +0000

    finished with media screen

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuIcon = document.getElementById('menu-icon');
+  const navList = document.querySelector('.nav__list');
+  const navList_2 = document.querySelector('.nav__list-2');
+
+  menuIcon.addEventListener('click', function () {
+    navList.classList.toggle('active');
+  });
+  menuIcon.addEventListener('click', function () {
+    navList_2.classList.toggle('active');
+  });
+});
